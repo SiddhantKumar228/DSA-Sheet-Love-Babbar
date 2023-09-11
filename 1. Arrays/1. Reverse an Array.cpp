dea5ec9c9016a7dfa9052c9ea5f0ce70f5a8ec6e
SiddhantKumar228 <siddhantsinha228@gmail.com>
2023-09-11 09:52:45 +0530

    Updated

--- 1. Arrays/1. Reverse an Array.cpp
+++ 1. Arrays/1. Reverse an Array.cpp
@@ -1,1 +1,48 @@
-#include <iostream>
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    string reverseWord(string str)
+    {
+        /******************************************** SOLUTION *************************************************/
+
+        int left = 0;
+        int right = str.size() - 1;
+
+        while (left < right)
+        {
+            swap(str[left], str[right]);
+            left++;
+            right--;
+        }
+        return str;
+
+        /*******************************************************************************************************/
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.reverseWord(s) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
