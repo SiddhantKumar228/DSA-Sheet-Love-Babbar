4a80cf9caf5ef2c5acd8f2397304fc4c0cb1507a
SiddhantKumar228 <siddhantsinha228@gmail.com>
2023-09-11 11:04:20 +0530

    Updated

---
+++ 3. Strings/1. Reverse a String.cpp
@@ -0,0 +1,51 @@
+/*
+
+Reverse a String
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(log(n))
+Constraints:
+1 <= N <= 105
+1 <= arr[i] <= 105
+1 <= K <= N
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string reverseWord(string str)
+    {
+        /******************************************** SOLUTION *************************************************/
+
+        int left = 0;
+        int right = str.size() - 1;
+
+        while (left < right)
+        {
+            swap(str[left], str[right]);
+            left++;
+            right--;
+        }
+        return str;
+
+        /*******************************************************************************************************/
+    }
+};
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.reverseWord(s) << endl;
+    }
+    return 0;
+}
